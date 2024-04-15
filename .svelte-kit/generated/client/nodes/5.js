96c856c658ccdd03dd94c70b18bdfef470aed2e3
Brayden Christensen <braydenchristensen6@gmail.com>
2024-04-14 20:14:40 -0600

    Add project to repo

---
+++ .svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/registry/+page.svelte";
---
+++ .svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/rsvp/+page.svelte";
