886ba8b257ec6e16f31ec187c5c6669996b730d9
Brayden Christensen <braydenchristensen6@gmail.com>
2024-07-15 18:06:50 -0600

    changed adapter

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,16 @@
 <script>
 </script>
 
+
 <div class="text-primary-500">
 	<div class="container mx-auto items-center justify-center space-y-10 p-2">
-		<div class="flex items-center justify-center text-4xl">
-			<h3>**About Brayden and Madeline**</h3>
+		<div class="flex items-center justify-center text-6xl font-nfRegular">
+			<h3>We're getting married!</h3>
 		</div>
 		<div class="flex items-center justify-center text-xl">
-			<p>**insert story**</p>
+			<p>
+				Brayden Christensen and Madeline Peterson are happy to announce their wedding on Saturday May 31st, 2025.
+			</p>
 		</div>
 	</div>
 	<br />
