4c16aa5bc64af786e8a016323ea96d78b9abbaa5
Brayden Christensen <braydenchristensen6@gmail.com>
2024-07-15 22:27:22 -0600

    1

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,9 +1,9 @@
-<script>
+<script lang="ts">
 	let spotifyLink =
 		'https://open.spotify.com/playlist/3cZWlFZhFbIsPRMxwE0Z0Z?si=feb4a52de40c4850&pt=069734137ccd443c481a7f71da35b3d5';
 	let registrySiteOne = '/';
 	let registrySiteTwo = '/';
-	let error = "/404"
+
 	let curr_date = new Date();
 	let wedding_date = new Date(2025, 4, 31, 15, 59, 59);
 
@@ -20,13 +20,13 @@
 	<div class="logo-cloud grid-cols-1 place-items-center content-center gap-5 p-3 lg:!grid-rows-3">
 		<a
 			class="logo-item variant-glass-surface size-2/4 rounded-xl duration-300 hover:variant-glass-primary hover:scale-110 hover:ease-in-out"
-			href={error}
+			href={registrySiteOne}
 		>
 			<span class="text-3xl text-primary-500">Registry Site</span>
 		</a>
 		<a
 			class="logo-item variant-glass-surface size-2/4 rounded-xl duration-300 hover:variant-glass-primary hover:scale-110 hover:ease-in-out"
-			href={error}
+			href={registrySiteTwo}
 		>
 			<span class="text-3xl text-primary-500">Registry Site</span>
 		</a>
